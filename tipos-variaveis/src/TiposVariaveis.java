9a8a4427fa2db47577ed20604313193f27763dab
alcarlucci <alcarlucci02@gmail.com>
2023-03-16 12:56:57 -0300

    Atualização TiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        byte meuByte = 100;
+        short meuShort = 15000;
+        int meuInt = 250000;
+        long meuLong = 1234567890;
+        float meuFloat = 35.78f;
+        double meuDouble = 12456.7894;
+        boolean meuBoolean = true;
+        char meuChar = 'A';
+        
+        //classe String para texto
+        String meuNome = "André Carlucci";
+
+        System.out.println("Tipos Primitivos:");
+        System.out.println(meuByte);
+        System.out.println(meuShort);
+        System.out.println(meuInt);
+        System.out.println(meuLong);
+        System.out.println(meuFloat);
+        System.out.println(meuDouble);
+        System.out.println(meuBoolean);
+        System.out.println(meuChar);
+
+        System.out.println("String 'meuNome': " + meuNome);
     }
+
 }
